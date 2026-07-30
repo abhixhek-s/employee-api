cff1a9a481d1f03fa7439b3be6e0fe13695b4d44
Abhishek S <Abhishek.S@distvm.com>
2026-07-30 17:56:57 +0530

    Initial commit

---
+++ employee_project/employee/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Employee(models.Model):
+
+    employee_name = models.CharField(max_length=100)
+    department = models.CharField(max_length=100)
+    date_of_joining = models.DateField()
+
+    def __str__(self):
+        return self.employee_name
