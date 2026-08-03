8e98cf271cb181cbcb3642afc920cb835043a39b
Abhishek S <Abhishek.S@distvm.com>
2026-08-03 14:22:20 +0530

    Changes in views

--- employee/views.py
+++ employee/views.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 
 from .models import Employee
@@ -6,7 +7,25 @@ from .serializers import EmployeeSerializer
 
 
 class EmployeeViewSet(viewsets.ModelViewSet):
-
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
     permission_classes = [IsAuthenticated]
+
+    def create(self, request, *args, **kwargs):
+        many = isinstance(request.data, list)
+
+        serializer = self.get_serializer(
+            data=request.data,
+            many=many
+        )
+
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+
+        headers = self.get_success_headers(serializer.data)
+
+        return Response(
+            serializer.data,
+            status=status.HTTP_201_CREATED,
+            headers=headers
+        )
